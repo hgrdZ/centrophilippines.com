91a3703e05662e953304fee1a6350cdf058b4aa0
Zedjosef <zedrickjosefivicente@iskolarngbayan.pup.edu.ph>
2025-10-14 17:52:55 +0800

    Updated tailwind.config.js

--- src/tailwind.config.js
+++ src/tailwind.config.js
@@ -3,6 +3,23 @@ module.exports = {
   content: [
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
+  safelist: [
+    'grid',
+    'grid-cols-1',
+    'md:grid-cols-2',
+    'gap-4',
+    'gap-6',
+    'gap-8',
+    'flex',
+    'flex-col',
+    'w-full',
+    'items-center',
+    'justify-between',
+    'rounded',
+    'px-3',
+    'px-4',
+    'py-2',
+  ],
   theme: {
     extend: {
       fontFamily: {
@@ -10,8 +27,7 @@ module.exports = {
         heading: ["Poppins", "ui-sans-serif", "system-ui"],
         gilroy: ["Gilroy", "ui-sans-serif", "system-ui"],
         avenir: ["Avenir", "ui-sans-serif", "system-ui"],
-        montserrat: ["Montserrat", "ui-sans-serif", "system-ui"], 
-
+        montserrat: ["Montserrat", "ui-sans-serif", "system-ui"],
       },
     },
   },
