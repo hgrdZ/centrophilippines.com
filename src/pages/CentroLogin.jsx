5b96af6d268c20f2b468609daaf83b349306c807
Zedjosef <zedrickjosefivicente@iskolarngbayan.pup.edu.ph>
2025-10-14 23:24:51 +0800

    Updated CentroLogin

--- src/pages/CentroLogin.jsx
+++ src/pages/CentroLogin.jsx
@@ -16,16 +16,18 @@ function CentroLogin({ setIsAuthenticated }) {
 
   // ===================== IMAGES =====================
   // Use process.env.PUBLIC_URL for deployment-safe paths (works on Vercel, GitHub, etc.)
- const images = [
-  "/images/volunteer1.png",
-  "/images/volunteer2.png",
-  "/images/volunteer3.png",
-  "/images/volunteer4.png",
-  "/images/volunteer5.png",
-  "/images/volunteer6.png",
-  "/images/volunteer7.png",
-];
-
+  const images = useMemo(
+    () => [
+      process.env.PUBLIC_URL + "/images/volunteer1.png",
+      process.env.PUBLIC_URL + "/images/volunteer2.png",
+      process.env.PUBLIC_URL + "/images/volunteer3.png",
+      process.env.PUBLIC_URL + "/images/volunteer4.png",
+      process.env.PUBLIC_URL + "/images/volunteer5.png",
+      process.env.PUBLIC_URL + "/images/volunteer6.png",
+      process.env.PUBLIC_URL + "/images/volunteer7.png",
+    ],
+    []
+  );
 
   const CentroLogo = process.env.PUBLIC_URL + "/images/CENTRO_Logo.png";
   const LoginIcon = process.env.PUBLIC_URL + "/images/login.svg";
@@ -133,7 +135,7 @@ function CentroLogin({ setIsAuthenticated }) {
     <div className="h-screen w-screen overflow-hidden relative font-montserrat bg-white opacity-100">
       <div className="flex h-full w-full">
         {/* LEFT SIDE - Auto Slideshow */}
-        <div className="w-1/2 relative hidden md:flex items-center justify-center overflow-hidden">
+<div className="w-1/2 relative sm:flex hidden items-center justify-center overflow-hidden">
           {/* Current Image */}
           <img
             src={currentImage}
